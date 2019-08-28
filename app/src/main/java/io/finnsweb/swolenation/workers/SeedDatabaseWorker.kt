de087a9f6afcb81908f1e3129b98a2739977ed94
Alex Finnarn <alex.finnarn@gmail.com>
2019-08-28 17:10:09 -0400

    add more about seeder

--- app/src/main/java/io/finnsweb/swolenation/workers/SeedDatabaseWorker.kt
+++ app/src/main/java/io/finnsweb/swolenation/workers/SeedDatabaseWorker.kt
@@ -1,2 +1,25 @@
 package io.finnsweb.swolenation.workers
 
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.coroutineScope
+import java.lang.Exception
+
+class SeedDatabaseWorker(
+    context: Context,
+    workerParams: WorkerParameters,
+) : CoroutineWorker(context, workerParams) {
+
+    private val TAG by lazy { SeedDatabaseWorker::class.java.simpleName }
+
+    override suspend fun doWork(): Result = coroutineScope {
+        try {
+            applicationContext.assets.open()
+        } catch (e: Exception) {
+            Log.e(TAG, "Error seeding database", e)
+            Result.failure()
+        }
+    }
+}
